61f639b6d16e1e50bbcf1e6aa2808b4a4a626382
Savannah C <cynomania@gmail.com>
2020-04-23 13:28:45 -0400

    setting up controllers

--- app/controllers/cryptids_controller.rb
+++ app/controllers/cryptids_controller.rb
@@ -1,2 +1,21 @@
 class CryptidsController < ApplicationController
+
+    def index
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    private
+
+    def cryptid_params
+        params.require(:cryptid).permit(:name, :size, :classification)
+    end
+
 end
--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,2 +1,21 @@
 class SightingsController < ApplicationController
+
+    def index
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    private
+
+    def sighting_params
+        params.require(:sighting).permit(:date, :location, :note, :cryptid_id)
+    end
+
 end
