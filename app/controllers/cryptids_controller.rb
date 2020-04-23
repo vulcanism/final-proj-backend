2eff8f04bd6eaea374719dbb873d550b790b0f28
Savannah C <cynomania@gmail.com>
2020-04-23 13:36:33 -0400

    cryptid controller actions

--- app/controllers/cryptids_controller.rb
+++ app/controllers/cryptids_controller.rb
@@ -1,21 +1,43 @@
 class CryptidsController < ApplicationController
+    before_action :set_cryptid, only: [:update, :destroy]
 
     def index
+        @cryptids = Cryptid.all
+        render json: @cryptids
     end
 
     def create
+        @cryptid = Cryptid.new(cryptid_params)
+        if @cryptid.save
+            render json: @cryptid
+        else
+            render json: {error: "Error creating cryptid"}
+        end
     end
 
     def show
+        @cryptid = Cryptid.find_by(id: params[:id])
+        render json: @cryptid
+    end
+
+    def update
+        @cryptid.update(cryptid_params)
+        head :no_content
     end
 
     def destroy
+        @cryptid.destroy
+        head :no_content
     end
 
     private
 
     def cryptid_params
         params.require(:cryptid).permit(:name, :size, :classification)
+    end
+
+    def set_cryptid
+        @cryptid = Cryptid.find_by(id: params[:id])
     end
 
 end
